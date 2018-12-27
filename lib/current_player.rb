04573f6f5ef5485be9208ff101081b26b02053cc
Mahbubur Rahman <mail@mahbub.me>
2018-12-27 06:36:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count( board )
   counter = 0
-  board.each { | player | player == "X" ||  }
+  board.each { | player | player == "X" || "O" }
 end
 def current_player( board )
   turn_count
