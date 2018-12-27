08a974461ce428ec3b005c885aad0fd65312553c
Mahbubur Rahman <mail@mahbub.me>
2018-12-27 06:37:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count( board )
   counter = 0
-  board.each { | player | counter += 1 player == "X" || "O" }
+  board.each { | player | counter += 1 if player == "X" || "O" }
 end
 def current_player( board )
   turn_count
